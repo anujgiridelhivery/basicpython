ac8c1a433a233e0be4e5a17f5d55164a83184ccb
anujgiridelhivery <40859432+anujgiridelhivery@users.noreply.github.com>
2018-08-10 03:51:32 +0530

    delete

--- pand.py
+++
@@ -1,9 +0,0 @@
-import csv
-import pandas as pd 
-df1 = pd.read_csv("24.csv",index_col = None, low_memory = False)
-df2 = pd.read_csv("23.csv",index_col = None, low_memory = False)
-df3 = pd.read_csv("22.csv",index_col = None, low_memory = False)
-df4 = pd.read_csv("21.csv",index_col = None, low_memory = False)
-df5 = pd.read_csv("20.csv",index_col = None, low_memory = False)
-df6 = pd.read_csv("19.csv",index_col = None, low_memory = False)
-df6 = pd.read_csv("25.csv",index_col = None, low_memory = False)
